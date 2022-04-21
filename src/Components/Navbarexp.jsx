eee97333eba189183ffd2d78431f068d7c2464fb
MdTauseef27 <mdtauseef27@gmail.com>
2022-04-21 16:51:35 +0530

    Added routing

--- src/Components/Navbarexp.jsx
+++ src/Components/Navbarexp.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import {Navbar,Nav} from "react-bootstrap";
+import {Home} from "./Home";
+import {Contactus} from "./Contactus";
+import {Aboutus} from "./Aboutus";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 export const Navbarexp=()=>{
     return(
         <div>
@@ -10,6 +14,23 @@ export const Navbarexp=()=>{
       <Nav.Link href="/aboutus">About Us</Nav.Link>
     </Nav>
    </Navbar>
+   <Router>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/contactus">
+            <Contactus />
+          </Route>
+          <Route path="/aboutus">
+            <Aboutus />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
         </div>
     )
 }
